50ac5af077ebd04ad07db286550ffee0d6cd8be3
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2017-05-30 12:33:38 +0100

    Day4_Geometric_Distribution_1
    Day4_Geometric_Distribution_1

---
+++ 10DaysStatistics/Day4_Geometric_Distribution_1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static double geometricProbability(int n, double p){
+        double r=0;
+        r = Math.pow((1-p),(n-1))*p;
+        return r;
+    }
+
+    public static void main(String[] args) {
+        //results:
+        double result=0;
+        DecimalFormat df = new DecimalFormat("#.###");
+        
+        //input:
+        double p = 1d/3d;//((double) 1/ (double) 3);
+        int n = 5;
+        
+        //calculations
+        //g(n,p) = (q^(n-1))*p
+        result = geometricProbability(n,p);
+        //System.out.println(result);
+        System.out.println(df.format(result));
+        
+    }
+}
